ede369a6cd0237d20a21621875ccaa649d6d20dd
Amit Gal <amitgal@hotmail.com>
2024-05-29 16:00:26 +0300

    Add JS click transition and sizing

--- js/main.js
+++ js/main.js
@@ -3,8 +3,20 @@ var ballSize = 100
 
 function onBallClick() {
   var elBall = document.querySelector('.ball')
-  ballSize += 50
+  ballSize < 400 ? (ballSize += 50) : (ballSize = 100)
   elBall.style.width = ballSize + 'px'
   elBall.style.height = ballSize + 'px'
+  elBall.style.transition = '1s'
   elBall.innerText = ballSize
+  //   if (ballSize < 400) {
+  //     ballSize += 50
+  //     elBall.style.width = ballSize + 'px'
+  //     elBall.style.height = ballSize + 'px'
+  //     elBall.innerText = ballSize
+  //   } else {
+  //     ballSize = 100
+  //     elBall.style.width = ballSize + 'px'
+  //     elBall.style.height = ballSize + 'px'
+  //     elBall.innerText = ballSize
+  //   }
 }
