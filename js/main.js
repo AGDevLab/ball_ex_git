176459cef1377ce3bfeff132e08afaab3d537b37
Amit Gal <amitgal@hotmail.com>
2024-05-29 19:33:17 +0300

    Add 4th ball

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var switchSelector
 
 function onBallClick(elBtnBall, maxDiameter) {
   var maxDiameter = 0
+  var randSizeBall = getRandomInt(20, 61)
+  var randBallColor = getRandomColor()
   // console.log(elBtnBall.getAttribute('data-id'))
   switchSelector = elBtnBall.getAttribute('data-id')
   // console.log(switchSelector)
@@ -18,15 +20,19 @@ function onBallClick(elBtnBall, maxDiameter) {
       maxDiameter = 400
       break
     case 'ball3':
-      console.log('ball3')
+      var elBall1 = document.querySelector('.ball1')
+      elBall1.style.backgroundColor = getRandomColor()
+      elBall1.style.width = ballSize + getRandomInt(20, 61) + 'px'
+      elBall1.style.height = ballSize + getRandomInt(20, 61) + 'px'
+      var elBall2 = document.querySelector('.ball2')
+      elBall2.style.backgroundColor = getRandomColor()
+      elBall2.style.width = ballSize + getRandomInt(20, 61) + 'px'
+      elBall2.style.height = ballSize + getRandomInt(20, 61) + 'px'
+      break
+    case 'ball4':
+      console.log('ball4')
       break
   }
-
-  // elBtnBall.getAttribute('data-id') === 'ball1'
-  //   ? (maxDiameter = 300)
-  //   : (maxDiameter = 400)
-  var randSizeBall = getRandomInt(20, 61)
-  var randBallColor = getRandomColor()
 
   ballSize < maxDiameter ? (ballSize += randSizeBall) : (ballSize = 100)
   elBtnBall.style.width = ballSize + 'px'
