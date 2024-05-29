3bb7d7a2cbccb489e7bcbcdd125e3402f5321f4a
Amit Gal <amitgal@hotmail.com>
2024-05-29 19:13:46 +0300

    Add ball and switch case

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 'use strict'
 var ballSize = 100
+var switchSelector
 
 function onBallClick(elBtnBall, maxDiameter) {
   var maxDiameter = 0
-  console.log(elBtnBall.getAttribute('data-id'))
+  // console.log(elBtnBall.getAttribute('data-id'))
+  switchSelector = elBtnBall.getAttribute('data-id')
+  // console.log(switchSelector)
 
-  elBtnBall.getAttribute('data-id') === 'ball1'
-    ? (maxDiameter = 300)
-    : (maxDiameter = 400)
+  switch (switchSelector) {
+    case 'ball1':
+      console.log('ball1')
+      maxDiameter = 300
+      break
+    case 'ball2':
+      console.log('ball2')
+      maxDiameter = 400
+      break
+    case 'ball3':
+      console.log('ball3')
+      break
+  }
+
+  // elBtnBall.getAttribute('data-id') === 'ball1'
+  //   ? (maxDiameter = 300)
+  //   : (maxDiameter = 400)
   var randSizeBall = getRandomInt(20, 61)
   var randBallColor = getRandomColor()
 
