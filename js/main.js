ca3998549998081354895b4f516618ec03245e33
Amit Gal <amitgal@hotmail.com>
2024-05-29 17:32:57 +0300

    Add 2nd ball and styling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 'use strict'
 var ballSize = 100
 
-function onBallClick() {
-  var elBall = document.querySelector('.ball')
+function onBallClick(elBtnBall) {
+  var elBall = elBtnBall
+  // var elBall = elBtnBall.querySelector('.ball1')
+  // var elBall = document.querySelector('.ball')
   var randSizeBall = getRandomInt(20, 61)
   var randBallColor = getRandomColor()
-  // console.log(randSizeBall)
 
   ballSize < 400 ? (ballSize += randSizeBall) : (ballSize = 100)
   elBall.style.width = ballSize + 'px'
