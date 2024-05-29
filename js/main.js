0954fb17b1693d7ac9f6f900cb593a1ac72786a7
Amit Gal <amitgal@hotmail.com>
2024-05-30 01:20:08 +0300

    Add html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,55 @@
 'use strict'
 var ballSize = 100
+var switchSelector
+var elBall1 = document.querySelector('.ball1')
+var elBall2 = document.querySelector('.ball2')
 
 function onBallClick(elBtnBall, maxDiameter) {
   var maxDiameter = 0
-  console.log(elBtnBall.getAttribute('data-id'))
-
-  elBtnBall.getAttribute('data-id') === 'ball1'
-    ? (maxDiameter = 300)
-    : (maxDiameter = 400)
+  var minDiamater = 100
   var randSizeBall = getRandomInt(20, 61)
   var randBallColor = getRandomColor()
+  // console.log(elBtnBall.getAttribute('data-id'))
+  switchSelector = elBtnBall.getAttribute('data-id')
+  // console.log(switchSelector)
+
+  switch (switchSelector) {
+    case 'ball1':
+      console.log('ball1')
+      maxDiameter = 300
+      break
+    case 'ball2':
+      console.log('ball2')
+      maxDiameter = 400
+      break
+    case 'ball3':
+      elBall1.style.backgroundColor = getRandomColor()
+      elBall1.style.width = ballSize + getRandomInt(20, 61) + 'px'
+      elBall1.style.height = ballSize + getRandomInt(20, 61) + 'px'
+      elBall1.style.transition = '1s'
+      elBall2.style.backgroundColor = getRandomColor()
+      elBall2.style.width = ballSize + getRandomInt(20, 61) + 'px'
+      elBall2.style.height = ballSize + getRandomInt(20, 61) + 'px'
+      elBall2.style.transition = '1s'
+      break
+    case 'ball4':
+      console.log('ball4')
+
+      elBall1.style.width = ballSize - getRandomInt(20, 61) + 'px'
+      elBall1.style.height = ballSize - getRandomInt(20, 61) + 'px'
+      elBall1.innerText = ballSize
+      elBall1.style.transition = '1s'
+
+      elBall2.style.width = ballSize - getRandomInt(20, 61) + 'px'
+      elBall2.style.height = ballSize - getRandomInt(20, 61) + 'px'
+      elBall2.innerText = ballSize
+      elBall2.style.transition = '1s'
+      break
+    case 'ball5':
+      var elBody = document.querySelector('body')
+      elBody.style.backgroundColor = getRandomColor()
+      break
+  }
 
   ballSize < maxDiameter ? (ballSize += randSizeBall) : (ballSize = 100)
   elBtnBall.style.width = ballSize + 'px'
